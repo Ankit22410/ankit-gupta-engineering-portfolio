b7760d778140dd01a4873e9073c4c946c72d4fb0
ANKIT GUPTA <ankit.22410@knit.ac.in>
2026-06-13 19:47:43 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'es2015',
-    minify: 'esbuild',
+    minify: 'esbuild'
     sourcemap: false,
     rollupOptions: {
       output: {
